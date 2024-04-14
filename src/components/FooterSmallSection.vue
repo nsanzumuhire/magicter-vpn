0787e319ea4ba0e3705e7d72f7b00e04f40c93e8
Nsanzumuhire Daniel <nsanzudaniel59@gmail.com>
2024-04-14 19:11:27 +0200

    (feature): Added small footer for Auth and Admin layouts

---
+++ src/components/FooterSmallSection.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="bg-white-300 w-full  xl:absolute xl:bottom-0">
+      <div class="flex w-full px-6 sm:px-8 lg:px-16">
+        <div class="flex divide-x divide-gray-500 my-6 space-x-2 py-1 w-full items-center justify-center">
+            <a class="text-sm text-black-500 cursor-pointer hover:underline">Terms of use</a>
+            <a class="text-sm pl-2 text-black-500  cursor-pointer hover:underline">Privacy policy</a>
+            <a class="text-sm pl-2 text-black-500">info@magicter.com</a>
+            <p class="text-gray-400 text-sm pl-2">©{{ new Date().getFullYear() }} - MagitecterVPN</p>
+          </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+
+  
+  export default {
+    components: {
+    },
+  };
+  </script>
--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -1,14 +1,15 @@
 <template>
-    <div class="flex flex-col h-screen w-full bg-white-300 border borde">
+    <div class="flex relative flex-col h-screen w-full bg-white-300 border borde">
         <div class="container flex w-full items-center justify-center  mx-auto mt-6 sm:mt-12">
             <main class="rounded-lg sm:pb-6 sm:pt-8 sm:px-6 bg-red flex flex-col bg-white-500 items-center w-[418px] shadow-t">
-                <div class="flex gap-2 items-center mb-16">
+                <router-link to="/" class="flex gap-2 items-center mb-16 cursor-pointer">
                     <img src="../assets/Logo.png" class="h-8 w-auto"  alt="Logo">
                     <h2 class="text-black-500 text-lg font-extrabold">Magicter <span class="text-purple-500">VPN</span></h2>
-                </div> 
+                </router-link> 
                 <router-view />
             </main>
         </div>
+        <FooterSmallSection/>
     </div>
     <!-- -->
 
@@ -16,5 +17,11 @@
 </template>
 
 <script>
+import FooterSmallSection from '../components/FooterSmallSection.vue'
+export default {
+    components: {
+        FooterSmallSection
+    }
+}
 
 </script>
